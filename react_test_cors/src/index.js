c824f04c0bae89c04c8b8984bbe776db7034df54
Isaac Ameh <67932483+isaacamehgreg@users.noreply.github.com>
2021-12-20 07:31:02 +0100

    push

--- react_test_cors/src/index.js
+++ react_test_cors/src/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import  ReactDOM  from 'react-dom';
 
 
+function getButtonText(){
+    return 'hi there'
+}
+
+const style = {
+    text: 'submit',
+    backgroundColor:'green'
+}
 
 const App = () => {
+
     return(
         <div>
+            <label htmlFor="name">Name</label> 
             <h1>Hello</h1>
             <p>here</p>
-            <button style={{backgroundColor:'red', color: 'white'}}> Submit</button>
+            <button style={{backgroundColor:'red', color: 'white'}}> {style.text} {getButtonText()}</button>
 
         </div>
      
